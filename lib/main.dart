9fd2f9f7c716cdf037138ce69651ad0a8bd5863f
aminmk99 <amirzakuchaki@gmail.com>
2023-02-12 21:49:19 +0330

    Added cards middle of screen

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,12 @@ class MyApp extends StatelessWidget {
             fontWeight: FontWeight.w300,
             color: Colors.black,
           ),
+          bodyText2: TextStyle(
+            fontFamily: 'dana',
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+            color: Colors.black,
+          ),
           headline2: TextStyle(
             fontFamily: 'dana',
             fontSize: 14,
@@ -136,6 +142,49 @@ myBody(BuildContext context) {
               ),
             ],
           ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          width: double.infinity,
+          height: 350,
+          child: ListView.builder(
+              itemCount: 50,
+              itemBuilder: (BuildContext context, int pos) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Container(
+                    width: double.infinity,
+                    height: 48,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          blurRadius: 2.0,
+                          color: Colors.grey,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'دلار',
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        Text(
+                          '46000',
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        Text(
+                          '+5',
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
         ),
       ],
     ),
